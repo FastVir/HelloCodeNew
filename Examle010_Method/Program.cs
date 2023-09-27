27d4f4ff68d863336009ece6924fee5c3084ba98
Dimitry Timofeev <fasvir32gar@gmail.com>
2023-09-27 16:06:38 +0300

    add file

---
+++ Examle010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 45, 55, 66, 777, 88888, 55, 1111111, 4 };
+
+int n = array.Length;
+int find = 55;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
